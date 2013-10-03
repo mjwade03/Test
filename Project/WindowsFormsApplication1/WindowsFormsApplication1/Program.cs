8811ac5d95b32fbc9915040496387f8cf69637f2
mjwade03 <kiie02@gmail.com>
2013-10-03 10:07:45 +0800

    test

--- Project/WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ Project/WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
