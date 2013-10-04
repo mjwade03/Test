be95ed1a478f088c5bd611f045f8b756d9ebd91a
mjwade03 <kiie02@gmail.com>
2013-10-04 09:16:49 +0800

    commit2

--- Project/WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ Project/WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApplication1
             int i = 0;
             i++;
             i++;
+            i++;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
